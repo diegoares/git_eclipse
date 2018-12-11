a0f6a3beeb1a570951ca9b3c8e66ee8d34d757ac
Diego Barreiro <diegotrb_@hotmail.com>
2018-12-11 11:55:08 +0100

    PUNTO1

---
+++ src/git/diego.java
@@ -0,0 +1,66 @@
+package git;
+
+public class diego {
+
+	private String nombre;
+	private String apellidos;
+	private String dni;
+	
+	
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+
+
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+
+
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+
+
+
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+
+
+
+
+	public String getDni() {
+		return dni;
+	}
+
+
+
+
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+
+
+
+
+	public void constructor(String nombre,String apellidos,String dni) {
+		this.nombre=nombre;
+		this.apellidos=apellidos;
+		this.nombre=dni;
+	}
+}
