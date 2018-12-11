a7b03371578ef67f1aeb57288cc49d757f3dbe29
Diego Barreiro <diegotrb_@hotmail.com>
2018-12-11 12:57:12 +0100

    PUNTO3

--- src/git/relog.java
+++ src/git/relog.java
@@ -1,5 +1,7 @@
 package git;
 
+import java.util.ArrayList;
+
 public class relog {
 	private int hora;
 	private int minutos;
@@ -51,5 +53,39 @@ public class relog {
 		this.hora=hora;
 		this.minutos=minutos;
 		this.segundos=segundos;
+	}
+	public void print(String Entrada) {
+		int numEquipos = 5;
+
+		ArrayList<String> liga = new ArrayList<>();
+		ArrayList<String> liga2 = new ArrayList<>();
+
+		System.out.println();
+
+		for(int i = 0; i < numEquipos; i++) {
+			System.out.print("Dime el equipo número " + (i + 1) + ": ");
+			liga.add(Entrada);
+		}
+
+		liga2 =  liga;
+
+		System.out.println("\n\tArrayList liga: " + liga);
+		System.out.println("\tArrayList liga2: " + liga2);
+
+		System.out.println("\n\tCantidad de valores del ArrayList liga: " + liga.size());
+
+		liga.remove(4);
+		System.out.println("\n\tPosición 4 del ArrayList liga borrado: " + liga);
+
+		System.out.print("\nDime un nuevo equipo para la posición 2 del ArrayList: ");
+		String posicion2 = Entrada;
+		liga.set(2, posicion2);
+		System.out.println("\n\tosicón 2 del ArrayList liga sustituido: " + liga);
+
+		System.out.print("\nDime el equipo que esta en la posicion 3 del Arralist: ");
+		String posicion3 = Entrada;
+		liga.remove(posicion3);
+		System.out.println("\n\tArrayList liga con la posición 3 borrada: " + liga);
+
 	}
 }
